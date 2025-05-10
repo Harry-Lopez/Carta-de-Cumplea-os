8db3b85e6b455b8ff650c92dc0cda9af9da71703
Harry-Lopez <enrique.l37708@gmail.com>
2025-05-09 18:46:58 -0600

    10-Mayo

---
+++ README.md
@@ -0,0 +1,2 @@
+# 14 De Febrero Proyect
+Proyecto sobre el 14 de Febrero (Uso personal)
---
+++ script 2.js
@@ -0,0 +1,28 @@
+const btnOpenElement = document.querySelector('#open')
+const btnCloseElement = document.querySelector('#close')
+
+btnOpenElement.addEventListener('click', ()=> {
+    const pestañaElement = document.querySelector('.pestaña')
+    pestañaElement.classList.add('open-pestaña')
+
+    setTimeout(()=>{
+        pestañaElement.style.zIndex = -2
+
+        const contenidoElement = document.querySelector('.contenido')
+    contenidoElement.classList.add('open-contenido')
+
+    },500)
+})
+
+btnCloseElement.addEventListener('click', ()=> {
+    const pestañaElement = document.querySelector('.pestaña')
+
+    const contenidoElement = document.querySelector('.contenido')
+    contenidoElement.classList.remove('open-contenido')
+    contenidoElement.classList.add('close-contenido')
+
+    setTimeout(()=>{
+        pestañaElement.style.zIndex = 0
+        pestañaElement.classList.remove('open-pestaña')
+    },500)
+})
